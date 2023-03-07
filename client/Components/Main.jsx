8eac180e3c496f36765933a239b5b0cc68a375a9
IRSHIT033 <mukherjeeirshit50@gmail.com>
2023-03-07 14:53:06 +0530

    testing cypress 2

--- client/Components/Main.jsx
+++ client/Components/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Main = () => {
-  const [firstOperand, setfirstOperand] = useState("");
+  const [firstOperand, setfirstOperand] = useState("2");
   const [SecondOperand, setSecondOperand] = useState("");
   const [ans, setans] = useState("");
   const getSum = () => {
